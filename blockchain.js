e61a32e054fa45fba04a9b981d3156b48de3da23
Kendrick Schuettler <keschuettler@gmail.com>
2017-10-12 15:32:16 -0400

    Added comments

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,4 @@
-
+/** Import dependencies */
 const app = require('express')();
 const bcrypt = require('bcrypt');
 const uuid4 = require('uuid/v4');
@@ -8,9 +8,11 @@ const rp = require('request-promise');
 
 
 const saltRounds = 10;
-// const salt = bcrypt.genSaltSync(saltRounds);
+
+// Need a constant salt rather than generated so that multiple nodes have same hash function.
 const salt = '$2a$10$gMk3Imt7iXvr9KEkBX0d/O';
 
+// Used to pay this node when a successful mining occurs
 const node_identifier = uuid4().replace('-', '');
 
 /** Blockchain Class */
@@ -20,6 +22,7 @@ class Blockchain {
     this.chain = [];
     this.currentTransactions = [];
     this.nodes = new Set();
+
     /** Create a genisis block */
     this._newBlock( 100, 1 );
   }
@@ -32,7 +35,7 @@ class Blockchain {
       timestamp: new Date(),
       transactions: this.currentTransactions,
       proof,
-      previous_hash: previous_hash || this.hash(this.chain[this.chain.length - 1])
+      previous_hash: previous_hash || this._hash(this.chain[this.chain.length - 1])
     }
 
     // Reset list of current transactions
@@ -43,26 +46,27 @@ class Blockchain {
 
   /** Adds a new transaction to the list of transactions */
   _newTransaction(sender, recipient, amount) {
+    
     this.currentTransactions.push({
       sender,
       recipient, 
       amount
     })
 
-    return this.lastBlock().index + 1;
+    return this._lastBlock().index + 1;
   }
   
   /** Hashes a block */
-  hash(block) {
+  _hash(block) {
     return bcrypt.hashSync(JSON.stringify(block), salt);
   }
 
   /** Returns the last block in the chain */
-  lastBlock () {
+  _lastBlock() {
     return this.chain[this.chain.length - 1]
   }
 
-  proofOfWork(lastProof) {
+  _proofOfWork(lastProof) {
   
     /** 
       Simple Proof of Work Algorithm:
@@ -70,13 +74,13 @@ class Blockchain {
         - p is the previous proof, and p' is the new proof
     */
     var proof = 0;
-    while(!this.validateProof(lastProof, proof)) {
+    while(!this._validateProof(lastProof, proof)) {
       proof++
     }
     return proof
   }
 
-  validateProof(last_proof, proof) {
+  _validateProof(last_proof, proof) {
     /**
       Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
     */
@@ -91,7 +95,7 @@ class Blockchain {
     :param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
     :return: None
   */
-  register_node(address) {
+  _registerNode(address) {
     var url = URL(address);
     this.nodes.add(url.host);
   }
@@ -101,8 +105,9 @@ class Blockchain {
     :param chain: <list> A blockchain
     :return: <bool> True if valid, False if not
   */
-  valid_chain(chain) {
-    console.log('Validating Chain', chain)
+  _isValidChain(chain) {
+    
+    console.log('Validating Chain... | ', chain)
     var lastBlock = chain[0];
     var currentIndex = 1;
 
@@ -111,14 +116,17 @@ class Blockchain {
       console.log('lastBlock: ', lastBlock);
       console.log('block: ', block);
       console.log('\n-----------------------\n');
+
       // Check that the block's hash is correct
-      if(block.previous_hash !== this.hash(lastBlock)) return false;
+      if(block.previous_hash !== this._hash(lastBlock)) return false;
+      
       // Check that proof of work is correct
-      if(!this.validateProof(lastBlock.proof, block.proof)) return false;
+      if(!this._validateProof(lastBlock.proof, block.proof)) return false;
 
       lastBlock = block;
       currentIndex++
     }
+
     console.log('Is Valid Chain!')
     return true
   }
@@ -128,16 +136,17 @@ class Blockchain {
    * by replacing our chain with the longest one in the network.
    * :return: <bool> True if our chain was replaced, False if not
    */
-  async resolve_conflicts() {
+  async _resolveClonflicts() {
     console.log("resolving conflicts")
+    
+    var newChain;
     var neighbors = [];
     this.nodes.forEach(node => neighbors.push(node));
-    var newChain;
 
     // We're only looking for chains longer than ours
     var maxLength = this.chain.length;
+    
     // Grab and verify the chains from all the nodes in our network
-
     return Promise.all(neighbors.map(node => {
       return rp.get(`http://${node}/chain`)
       .then((data) => {
@@ -149,42 +158,39 @@ class Blockchain {
       })
     }))
     .then(neighborChains => {
-  
       for(let i = 0; i < neighborChains.length; i++) {
-        var node = neighborChains[i]
-        if(node) {
-          var length = node.length;
-          var chain = node.chain;
-  
-          if(length > maxLength && this.valid_chain(chain)) {
-            maxLength = length;
-            newChain = chain;
-          }
+        var node = neighborChains[i]            
+        
+        // Check that neighbor node exists and has a valid chain longer than this node
+        if(node && node.length > maxLength && this._isValidChain(node.chain)) {
+          maxLength = node.length;
+          newChain = node.chain;
         }
       }
       
+      // If new, longer chain is found, replace the chain on this node with that chain.
       if(newChain) {
         this.chain = newChain;
         return true;
       }
   
       return false;
-    
     })
   }
 }
 
+/** Initiate a new blockchain on this node */
 const blockchain = new Blockchain()
 
 /** Express Configuration */
-app.set("port", 5800);
+app.set("port", 4800);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/mine', function(req, res) {
-  (function mine() {
-    const last_block = blockchain.lastBlock();
-    proof = blockchain.proofOfWork(last_block.proof)
+/** Mine a new block */
+app.get('/mine', function mine(req, res) {
+    const last_block = blockchain._lastBlock();
+    var proof = blockchain._proofOfWork(last_block.proof)
 
     blockchain._newTransaction({
       sender: 0,
@@ -192,7 +198,7 @@ app.get('/mine', function(req, res) {
       amount: 1
     })
 
-    const block = {index, transactions, proof, previous_hash} = blockchain._newBlock(proof);
+    const {index, transactions, proof, previous_hash} = blockchain._newBlock(proof);
 
     res.status(200).send(JSON.stringify({
       message: "New block forged",
@@ -201,11 +207,10 @@ app.get('/mine', function(req, res) {
       proof, 
       previous_hash
     }));
-  })()
 })
 
-app.post('/transactions/new', function(req, res) {
-  (function new_transaction(){
+/** Enter a new transaction to this node */
+app.post('/transactions/new', function newTransaction(req, res) {
     const values = {sender, recipient, amount} = req.body;
     const required = ['sender', 'recipient', 'amount'];
     
@@ -221,39 +226,34 @@ app.post('/transactions/new', function(req, res) {
     const index = blockchain._newTransaction(sender, recipient, amount);
     
     res.status(201).send(`Transaction will be added to Block ${index}`);
-  })()
 })
 
-app.get('/chain', function(req, res){
-  (function full_chain(){
-    var response = {
+/** Retrieve full chain on this node */
+app.get('/chain', function retrieveFullChain(req, res){
+    res.status(200).send({
       chain: blockchain.chain,
       length: blockchain.chain.length
-    }
-    
-    res.status(200).send(response)
-  })()
+    })
 })
 
-app.post('/nodes/register', function(req, res) {
-  (function registerNode(){
+/** Register a new list of nodes with this node to expand the current network. */
+app.post('/nodes/register', function registerNodes(req, res) {
     var { nodes } = req.body;
-    if(nodes === undefined) {
+    if(!nodes) {
       res.statusCode(400).send("Please supply a valid list of nodes")
     }
 
-    nodes.forEach(node => blockchain.register_node(node));
+    nodes.forEach(node => blockchain._registerNode(node));
     res.send({
       message: "New nodes have been added",
-      total_nodes: [...blockchain.nodes]
+      total_nodes: [...blockchain.nodes] // Destructure a Set into an Array
     })
-  }())
 })
 
-app.get('/nodes/resolve', function(req, res) {
-  ( async function consensus(){
-    var response = {};
-    var replaced = await blockchain.resolve_conflicts();
+/** Find consensus with registered nodes (i.e. find the correct chain via PoW calculation) */
+app.get('/nodes/resolve', async function findConcensus(req, res) {
+    var response;
+    var replaced = await blockchain._resolveClonflicts(); // Async function because _resolveConflicts makes numerous server calls
 
     if(replaced) {
       response = {
@@ -267,9 +267,9 @@ app.get('/nodes/resolve', function(req, res) {
       }
     }
     res.status(200).send(response);
-  }())
 })
 
+/** Start server */
 app.listen(app.get("port"), () => {
   console.log(` App is running at http://localhost:${app.get("port")}`);
   console.log(" Press CTRL-C to stop\n");
